6c491b0afb2b5c23e2f039726c0aa34006a4b7c9
Yann Braga <yannbf@gmail.com>
2017-10-20 00:51:29 -0300

    feat(lazyloading): use lazyloading on all pages
    Resolves #30

--- src/pages/user-search/user-search.module.ts
+++ src/pages/user-search/user-search.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { UserSearchPage } from './user-search';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
     UserSearchPage,
   ],
   imports: [
+    ComponentsModule,
     IonicPageModule.forChild(UserSearchPage),
   ],
 })
