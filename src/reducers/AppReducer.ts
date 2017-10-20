90fc2dcda8b1f1003f12d67ebb1e25b79aae2b2d
Andre AS <andre.santos0906@gmail.com>
2017-10-20 01:02:21 -0200

    removing spaces

--- src/reducers/AppReducer.ts
+++ src/reducers/AppReducer.ts
@@ -5,17 +5,17 @@ export const DECREMENT = 'DECREMENT';
 export const RESET = 'RESET';
 
 export function AppReducer(state: number = 0, action: Action) {
-	switch (action.type) {
-		case INCREMENT:
-			return state + 1;
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
 
-		case DECREMENT:
-			return state - 1;
+    case DECREMENT:
+      return state - 1;
 
-		case RESET:
-			return 0;
+    case RESET:
+      return 0;
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 }
