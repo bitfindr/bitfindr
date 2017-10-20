6ab7ac56056c9e24e382f0523043ecc72fb6ddf2
Yann Braga <yannbf@gmail.com>
2017-10-20 01:12:40 -0300

    feat(clipboard): create provider for clipboard to work on web and native
    Resolves #32

---
+++ src/providers/clipboard/clipboard.provider.ts
@@ -0,0 +1,26 @@
+import { Platform } from 'ionic-angular';
+import { Injectable, Renderer } from '@angular/core';
+import { Clipboard } from '@ionic-native/clipboard';
+import { ClipboardService } from 'ngx-clipboard';
+
+@Injectable()
+export class ClipboardProvider {
+
+  constructor(
+    private platform: Platform,
+    private nativeClipboard: Clipboard,
+    private jsClipboard: ClipboardService
+  ) { }
+
+  copy(content: string, renderer: Renderer) {
+    return this.platform.is('cordova') ? this.nativeCopy(content) : this.jsCopy(content, renderer);
+  }
+
+  nativeCopy(content) {
+    return this.nativeClipboard.copy(content);
+  }
+
+  jsCopy(content, renderer) {
+    return Promise.resolve(this.jsClipboard.copyFromContent(content, renderer));
+  }
+}
