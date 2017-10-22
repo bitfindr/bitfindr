9d5034d831b767e0566877994d9df88f3bf1d753
Khaled Shaaban <kshaaban@outlook.com>
2017-10-21 22:13:42 -0400

    feat(signup): add registration logic

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,21 +1,53 @@
+import { UserCredentials } from './../../shared/models/auth';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, AbstractControl } from '@angular/forms';
 
-@IonicPage()
+import { CustomValidators } from './../../shared/utils/custom-validators';
+import { AuthFacade } from './../../state/auth/auth.facade';
+
+@IonicPage({
+  segment: 'signup',
+  defaultHistory: ['LoginPage'],
+})
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html'
 })
 export class SignupPage {
-  signupForm: any;
-  email: AbstractControl;
-  password: AbstractControl;
-  passwordConfirm: AbstractControl;
-  firstName: AbstractControl;
-  lastName: AbstractControl;
+  signupForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController,
+    private formBuilder: FormBuilder,
+    private authFacade: AuthFacade
+  ) {
+    this.signupForm = formBuilder.group({
+      /**
+       * TODO (Khaled) Should we capture "profile" data on a separate page
+       * because of Firebase?
+       * See component template
+       */
+      // firstName: ['', Validators.required],
+      // lastName: ['', Validators.required],
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email,
+      ])],
+      password: formBuilder.group({
+        value: ['', Validators.required],
+        confirmed: ['', Validators.required],
+      }, { validator: CustomValidators.equalMatcher('value', 'confirmed') }),
+    });
+  }
 
-  signup() {}
+  signup() {
+    const formValue = this.signupForm.value;
+    const credentials: UserCredentials = {
+      email: formValue.email,
+      password: formValue.password.value
+    };
+
+    this.authFacade.signup(credentials);
+  }
 }
