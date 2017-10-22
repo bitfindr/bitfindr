bf3c6d169d166fb176ad0ef6215be05179143aeb
Khaled Shaaban <kshaaban@outlook.com>
2017-10-21 22:13:20 -0400

    feat(auth): set application rootPage based on current user auth state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,23 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AuthFacade } from './../state/auth/auth.facade';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = 'LoginPage';
+  rootPage: string;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    authFacade: AuthFacade
+  ) {
+    authFacade.authUser$
+      .subscribe(authUser => (this.rootPage = authUser ? 'TabsPage' : 'LoginPage'));
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
