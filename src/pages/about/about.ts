fcf3b397b6fd49a43c40475445e2b5c6e046ab2c
Khaled Shaaban <kshaaban@outlook.com>
2017-10-21 21:58:14 -0400

    refactor(state-management): remove old unused ngrx code

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs/Observable';
-import { INCREMENT, DECREMENT, RESET } from '../../reducers/AppReducer';
-
-interface AppState {
-  counter: number;
-}
 
 @Component({
   selector: 'page-about',
@@ -14,29 +7,8 @@ interface AppState {
 })
 export class AboutPage {
 
-  counter: Observable<number>;
-
   constructor(
     public navCtrl: NavController,
-    private store: Store<AppState>,
-  ) {
-    this.counter = store.select('counter');
-  }
-
-  increment() {
-    this.store.dispatch({ type: INCREMENT });
-  }
-
-  decrement() {
-    this.store.dispatch({ type: DECREMENT });
-  }
-
-  reset() {
-    this.store.dispatch({ type: RESET });
-  }
-
-  getValue() {
-    console.log(this.store);
-  }
+  ) { }
 
 }
