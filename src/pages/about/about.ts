978b334075c3671a4213d6fb920ebfb1a47cd2d7
Andre AS <andre.santos0906@gmail.com>
2017-10-22 12:27:00 -0200

    fix(import statement): removing a wrong import from a merge

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { INCREMENT, DECREMENT, RESET } from '../../reducers/AppReducer';
 
 interface AppState {
   counter: number;
