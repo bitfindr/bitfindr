20952d7a157fdacbc146fb6c3e7835654e883dbc
Khaled Shaaban <kshaaban@outlook.com>
2017-10-21 22:07:50 -0400

    feat(signup): add AuthProvider logic
    Signup & check user auth state logic

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,18 +1,58 @@
+import { FirebaseUserProfile, FirebaseBasicProfile } from './../../shared/models/auth';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { fromPromise } from 'rxjs/observable/fromPromise';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the AuthProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello AuthProvider Provider');
+  constructor(
+    private afAuth: AngularFireAuth
+  ) { }
+
+  checkAuthState() {
+    return this.afAuth.authState
+      .take(1)
+      .map(response => this.extractUserProfile(response));
   }
 
+  signup(userCredentials: { email: string, password: string }) {
+    const { email, password } = userCredentials;
+
+    const signupPromise = this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+
+    return fromPromise(signupPromise)
+      .map(response => this.extractUserProfile(response));
+  }
+
+  private extractUserProfile(data: any): FirebaseUserProfile {
+    if (data && !data.uid) {
+      throw Error('Invalid User Data');
+    } else if (!data) {
+      return data;
+    }
+
+    const providerData: FirebaseBasicProfile[] = [];
+    if (data.providerData.length) {
+      data.providerData.forEach(provider => {
+        const providerProfile = { ...provider };
+        providerData.push(providerProfile);
+      });
+    }
+
+    const profile: FirebaseUserProfile = {
+      displayName: data.displayName || null,
+      email: data.email || null,
+      emailVerified: data.emailVerified || false,
+      isAnonymous: data.isAnonymous || false,
+      photoURL: data.photoURL || null,
+      providerData,
+      providerId: data.providerId,
+      refreshToken: data.refreshToken,
+      uid: data.uid
+    };
+
+    return profile;
+  }
 }
