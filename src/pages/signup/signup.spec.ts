af03230b97d7244b2b5324bae5567f1aba226996
Khaled Shaaban <kshaaban@outlook.com>
2017-10-23 22:34:00 -0400

    fix(unit-testing): create AuthFacadeMock
    Without the mock, tests for SignupPage were broken due to missing dependencies.

--- src/pages/signup/signup.spec.ts
+++ src/pages/signup/signup.spec.ts
@@ -1,17 +1,21 @@
 import { ComponentFixture, async } from '@angular/core/testing';
-import { TestUtils } from './../../test';
+import { TestUtils } from './../../../test';
 import { SignupPage } from './signup';
+import { DebugElement } from '@angular/core';
 
+let debugEl: DebugElement = null;
 let fixture: ComponentFixture<SignupPage> = null;
 let instance: SignupPage = null;
+let nativeEl: HTMLElement = null;
 
 describe('Pages: SignupPage', () => {
-
   beforeEach(async(() => {
     TestUtils.beforeEachCompiler([SignupPage])
       .then(compiled => {
+        debugEl = compiled.debugEl;
         fixture = compiled.fixture;
         instance = compiled.instance;
+        nativeEl = compiled.nativeEl;
       });
   }));
 
