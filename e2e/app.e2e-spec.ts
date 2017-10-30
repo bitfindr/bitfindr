8fa2b28d2c360c4b96990714b9fa539c20319980
Khaled Shaaban <kshaaban@outlook.com>
2017-10-29 22:40:18 -0400

    test(e2e): workaround ngZone not synchronizing with protractor
    Seems to be due to usage of Firebase and websockets? They run inside the NgZone and cause it to never be "ready" or stable?
    
    https://github.com/angular/angular/issues/11853

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,13 +2,16 @@ import { browser, element, by } from 'protractor';
 
 describe('MyApp', () => {
 
-  beforeEach(() => {
-    browser.get('');
+  beforeEach(done => {
+    browser.ignoreSynchronization = true;
+    browser.get('')
+      .then(done);
   });
 
-  it('should have a title', () => {
-    browser.getTitle().then(title => expect(title).toEqual('Bitfindr'));
-    done();
+  it('should have a title', done => {
+    browser.getTitle()
+      .then(title => expect(title).toEqual('Bitfindr'))
+      .then(done);
   });
 
 });
